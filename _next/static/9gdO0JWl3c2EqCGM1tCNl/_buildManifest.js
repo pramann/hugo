ae3b04e9c438caf3a0fdf795d11290a1f3674f50
pramann <pramann@concedra.de>
2025-08-22 11:52:29 +0000

    Update from https://github.com/pramann/nextjs/commit/30df3a8168a0bbfae4fe3f3d913d49c5cd4d3939

---
+++ _next/static/9gdO0JWl3c2EqCGM1tCNl/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/f8b9d61d63650f9b.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/c373c21f2887fff8.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/50d3930286471d60.js",
+  "static/chunks/7372016b15b6856d.js",
+  "static/chunks/turbopack-7b2768f339f1f16c.js"
+])
